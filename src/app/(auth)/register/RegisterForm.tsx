f0bd7c771ef239d3cee6e70509f3962de835bff4
Collen Godfrey <cpgodf@gmail.com>
2024-09-25 11:43:28 -0400

    adds register page

--- src/app/(auth)/register/RegisterForm.tsx
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -1,5 +1,75 @@
+"use client";
+
+import { registerSchema, RegisterSchema } from "@/lib/schemas/registerSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Card, CardHeader, CardBody, Button, Input } from "@nextui-org/react";
+
 import React from "react";
+import { useForm } from "react-hook-form";
+import { GiPadlock } from "react-icons/gi";
 
 export default function RegisterForm() {
-  return <div>This is a Register Form</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema),
+    mode: "onTouched",
+  });
+  const onSubmit = (data: RegisterSchema) => {
+    console.log(data);
+  };
+  return (
+    <Card className="w-2/5 mx-auto ">
+      <CardHeader className="flex flex-col items-center justify-center">
+        <div className="flex flex-col gap-2 items-center text-secondary">
+          <div className="flex flex-row items-center gap-3 ">
+            <GiPadlock size={30} />
+            <h1 className="text-3xl">Register</h1>
+          </div>
+          <p className="text-neutral-500">Welcome to next match</p>
+        </div>
+      </CardHeader>
+      <CardBody>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="space-y-4">
+            <Input
+              defaultValue=""
+              label="Name"
+              variant="bordered"
+              {...register("name")}
+              isInvalid={!!errors.name}
+              errorMessage={errors.name?.message}
+            />
+            <Input
+              defaultValue=""
+              label="Email"
+              variant="bordered"
+              {...register("email")}
+              isInvalid={!!errors.email}
+              errorMessage={errors.email?.message}
+            />
+            <Input
+              defaultValue=""
+              label="Password"
+              type="password"
+              variant="bordered"
+              {...register("password")}
+              isInvalid={!!errors.password}
+              errorMessage={errors.password?.message}
+            />
+            <Button
+              isDisabled={!isValid}
+              fullWidth
+              color="secondary"
+              type="submit"
+            >
+              Register
+            </Button>
+          </div>
+        </form>
+      </CardBody>
+    </Card>
+  );
 }
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,7 +3,7 @@ import RegisterForm from "./RegisterForm";
 
 export default function Register() {
   return (
-    <div>
+    <div className="flex items-center justify-center align-middle verticle-center ">
       <RegisterForm />
     </div>
   );
